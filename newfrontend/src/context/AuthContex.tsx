64b08a14abdbb7c0e7a6951f786e9f3962200f66
BaleusBlueberry <ssaasshh29@gmail.com>
2024-10-18 02:03:52 +0300

    implemented login and register logic

--- newfrontend/src/context/AuthContex.tsx
+++ newfrontend/src/context/AuthContex.tsx
@@ -1,10 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 interface AuthContextType {
   isLoggedIn: boolean;
   token: string;
   login: (token: string) => void;
   logout: () => void;
+  authChecked: boolean; // Added to track when the authentication check is completed
 }
 
 const initialValues: AuthContextType = {
@@ -12,25 +13,46 @@ const initialValues: AuthContextType = {
   token: "",
   login: () => {},
   logout: () => {},
+  authChecked: false, // Initially false
 };
+
 const AuthContext = createContext(initialValues);
 
 function AuthProvider(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState("");
+  const [authChecked, setAuthChecked] = useState(false); // Track if the auth check is complete
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("user"); // Retrieve token from localStorage
+
+    if (storedToken) {
+      setIsLoggedIn(true); // Set isLoggedIn to true if token is found
+      setToken(storedToken); // Set token state
+      console.log("logged in");
+    } else {
+      setIsLoggedIn(false); // Set isLoggedIn to false if token not found
+      console.log("not logged in");
+    }
+    setAuthChecked(true); // Indicate that the authentication check is complete
+  }, []);
 
   function login(token: string) {
     setIsLoggedIn(true);
     setToken(token);
+    localStorage.setItem("user", token); // Store token in localStorage during login
   }
 
   function logout() {
     setIsLoggedIn(false);
     setToken("");
+    localStorage.removeItem("user"); // Remove token from localStorage during logout
   }
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, token, login, logout }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, token, login, logout, authChecked }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
--- newfrontend/src/routes/Register.tsx
+++ newfrontend/src/routes/Register.tsx
@@ -1,17 +1,31 @@
-import { Field, Form, Formik } from "formik";
-import { useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useContext, useEffect, useState } from "react";
 import * as Yup from "yup";
 import Spinner from "../components/Spinner";
 import { dialogs } from "../dialogs/dialogs";
+import auth from "../services/auth-service";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContex";
 
 const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string>();
+  const { isLoggedIn, authChecked } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authChecked) {
+      if (isLoggedIn) {
+        navigate("/");
+      } // Redirect if the user is logged in
+    }
+  }, [authChecked, isLoggedIn, navigate]);
 
   const validationSchema = Yup.object({
-    email: Yup.string().email("Bad Email!").required("The email is required"),
-    username: Yup.string().required().min(2).max(20),
-    password: Yup.string()
+    Email: Yup.string().email("Bad Email!").required("The Email is required"),
+    Username: Yup.string().required().min(2).max(20),
+    Password: Yup.string()
       .required()
       .min(8)
       .max(20)
@@ -19,89 +33,106 @@ const Register = () => {
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_-]).{8,30}$/,
         "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
       ),
-    confirmPassword: Yup.string()
+    PasswordConfirm: Yup.string()
       .required()
-      .oneOf([Yup.ref("password")], "Passwords must match"),
+      .oneOf([Yup.ref("Password")], "Passwords must match"),
   });
 
   const initialValues = {
-    email: "",
-    username: "",
-    password: "",
-    confirmPassword: "",
+    Email: "",
+    Username: "",
+    Password: "",
+    PasswordConfirm: "",
   };
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={() => {
+      onSubmit={(o) => {
         setIsLoading(true);
-        dialogs.success("Registered successfully!!!!");
-
-        //async task
-        setTimeout(() => {
-          setIsLoading(false);
-          setError(null);
-        }, 2000);
+        auth
+          .register(o.Email, o.Username, o.Password, o.PasswordConfirm)
+          .then(() => {
+            dialogs.success("Registered successfully!!!!");
+            navigate("/login");
+          })
+          .catch((error) => {
+            setError(error.message);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
       }}
     >
       <Form className="flex flex-col items-center">
         {isLoading && <Spinner />}
         {error && <p className="text-red-500">{error}</p>}
         <div className="font-extralight form-group flex flex-col gap-2 w-1/2 mx-auto text-lg my-4">
-          <label htmlFor="username">User Name</label>
+          <label htmlFor="Username">User Name</label>
           <Field
-            name="username"
+            name="Username"
             type="text"
-            id="username"
+            id="Username"
             className="rounded-md hover:border-2 border-2 px-2 py-2"
           />
-          {/* <ErrorMessage
-            name="username"
-            component="div"
-            className="text-red-500"
-          /> */}
+          <div className="h-0">
+            <ErrorMessage
+              name="Username"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
         </div>
 
         <div className="font-extralight form-group flex flex-col gap-2 w-1/2 mx-auto text-lg my-4">
-          <label htmlFor="email">Email Address</label>
+          <label htmlFor="Email">Email Address</label>
           <Field
-            name="email"
-            type="email"
-            id="email"
+            name="Email"
+            type="Email"
+            id="Email"
             className="rounded-md hover:border-2 border-2 px-2 py-2"
           />
-          {/* <ErrorMessage name="email" component="div" className="text-red-500" /> */}
+          <div className="h-0">
+            <ErrorMessage
+              name="Email"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
         </div>
 
         <div className="font-extralight form-group flex flex-col gap-2 w-1/2 mx-auto text-lg my-4">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="Password">Password</label>
           <Field
-            name="password"
-            type="password"
-            id="password"
+            name="Password"
+            type="Password"
+            id="Password"
             className="rounded-md hover:border-2 border-2 px-2 py-2"
           />
-          {/* <ErrorMessage
-            name="password"
-            component="div"
-            className="text-red-500"
-          /> */}
+          <div className="h-0">
+            <ErrorMessage
+              name="Password"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
         </div>
 
         <div className="font-extralight form-group flex flex-col gap-2 w-1/2 mx-auto text-lg my-4">
-          <label htmlFor="confirmPassword">Confirm Password</label>
+          <label htmlFor="PasswordConfirm">Confirm Password</label>
           <Field
-            name="confirmPassword"
-            type="password"
-            id="confirmPassword"
+            name="PasswordConfirm"
+            type="Password"
+            id="PasswordConfirm"
             className="rounded-md hover:border-2 border-2 px-2 py-2"
           />
-          {/* <ErrorMessage
-            name="confirmPassword"
-            component="div"
-            className="text-red-500"
-          /> */}
+          <div className="h-1">
+            <ErrorMessage
+              name="PasswordConfirm"
+              component="div"
+              className="text-red-500"
+            />
+          </div>
         </div>
 
         <button
